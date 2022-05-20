b2156d973909fb1b5678eb80663d5203a1de0726
Budschie <jul.kp06@gmail.com>
2022-05-20 18:01:56 +0200

    Language files are now fully data generated.

--- src/main/java/de/budschie/untitledtrollmod/data_gen/LanguageDataProvider.java
+++ src/main/java/de/budschie/untitledtrollmod/data_gen/LanguageDataProvider.java
@@ -1,9 +1,14 @@
 package de.budschie.untitledtrollmod.data_gen;
 
+import java.util.function.Supplier;
+
 import de.budschie.untitledtrollmod.blocks.BlockRegistry;
+import de.budschie.untitledtrollmod.effects.MobEffectRegistry;
+import de.budschie.untitledtrollmod.effects.PotionRegistry;
 import de.budschie.untitledtrollmod.entities.EntityRegistry;
 import de.budschie.untitledtrollmod.items.ItemRegistry;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.world.item.alchemy.Potion;
 import net.minecraftforge.common.data.LanguageProvider;
 
 public class LanguageDataProvider extends LanguageProvider
@@ -24,5 +29,17 @@ public class LanguageDataProvider extends LanguageProvider
 		addEntityType(() -> EntityRegistry.ROCKET_CREEPER.get(), "Rocket Creeper");
 		addEntityType(() -> EntityRegistry.TROLL_TNT.get(), "Troll TNT");
 		addEntityType(() -> EntityRegistry.SHEEP_HOPPER.get(), "Sheep Hopper");
+		
+		addEffect(() -> MobEffectRegistry.JESUS_JUICE_EFFECT.get(), "Jesus");
+		
+		addPotionEffectName(() -> PotionRegistry.JESUS_JUICE.get(), "Jesus Juice");
+		addPotionEffectName(() -> PotionRegistry.LONG_JESUS_JUICE.get(), "Long Jesus Juice");
+	}
+	
+	public void addPotionEffectName(Supplier<Potion> potion, String name)
+	{
+		add("item.minecraft.potion.effect." + potion.get().getRegistryName().getPath() , name);
+		add("item.minecraft.lingering_potion.effect." + potion.get().getRegistryName().getPath() , "Lingering " + name);
+		add("item.minecraft.splash_potion.effect." + potion.get().getRegistryName().getPath() , "Splashable " + name);
 	}
 }
