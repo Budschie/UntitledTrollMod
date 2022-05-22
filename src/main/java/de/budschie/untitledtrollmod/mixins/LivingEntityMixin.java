8ba5c3a7940aa222837ac10925812bcb027c552a
Budschie <jul.kp06@gmail.com>
2022-05-22 12:31:35 +0200

    Ported to 1.18.2

--- src/main/java/de/budschie/untitledtrollmod/mixins/LivingEntityMixin.java
+++ src/main/java/de/budschie/untitledtrollmod/mixins/LivingEntityMixin.java
@@ -8,17 +8,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import de.budschie.untitledtrollmod.effects.MobEffectRegistry;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin
 {
-	@Inject(method = "canStandOnFluid(Lnet/minecraft/world/level/material/Fluid;)Z", at = @At("HEAD"), cancellable = true)
-	private void canStandOnFluidMixin(Fluid fluid, CallbackInfoReturnable<Boolean> callback)
+	@Inject(method = "canStandOnFluid(Lnet/minecraft/world/level/material/FluidState;)Z", at = @At("HEAD"), cancellable = true)
+	private void canStandOnFluidMixin(FluidState fluid, CallbackInfoReturnable<Boolean> callback)
 	{
 		LivingEntity living = ((LivingEntity)((Object)this));
 		
-		if(living.hasEffect(MobEffectRegistry.JESUS_JUICE_EFFECT.get()) && fluid == Fluids.WATER)
+		if(living.hasEffect(MobEffectRegistry.JESUS_JUICE_EFFECT.get()) && fluid.getType() == Fluids.WATER)
 		{
 			callback.setReturnValue(true);
 		}
